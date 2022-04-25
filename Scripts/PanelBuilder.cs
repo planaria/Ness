c5a849c89db0f36b55545e13c7646d8913a4fe6f
Suzuki Kojiro <pn.suzuki.kojiro@gmail.com>
2022-04-25 15:24:04 +0900

    Add TwinPanelBuilder

--- Scripts/PanelBuilder.cs
+++ Scripts/PanelBuilder.cs
@@ -10,6 +10,7 @@ namespace SuzuFactory.Ness
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class PanelBuilder : UdonSharpBehaviour
     {
+        public bool twin = false;
         public TextAsset data;
         private Transform spawn;
         private string[] lines;
@@ -37,12 +38,9 @@ namespace SuzuFactory.Ness
 
             for (int i = 0; i < lines.Length; ++i)
             {
-                if (lines[i] != "")
+                if (!string.IsNullOrWhiteSpace(lines[i]))
                 {
-                    if (i != numLines)
-                    {
-                        lines[numLines++] = lines[i];
-                    }
+                    lines[numLines++] = lines[i];
                 }
             }
 
@@ -69,22 +67,75 @@ namespace SuzuFactory.Ness
 
         public override void Interact()
         {
-            var data = GetRandomData();
-
             var pool = transform.Find("/PanelPool").GetComponent<PanelPool>();
-            var obj = pool.TryToSpawn();
 
-            if (obj != null)
+            if (twin)
             {
-                obj.transform.SetPositionAndRotation(spawn.position, spawn.rotation);
+                var baseIndex = Random.Range(0, lines.Length / 2 - 1);
+                var index1 = baseIndex * 2;
+                var index2 = baseIndex * 2 + 1;
 
-                var sync = (VRCObjectSync)obj.GetComponent(typeof(VRCObjectSync));
-                sync.FlagDiscontinuity();
+                var data1 = lines[index1];
+                var data2 = lines[index2];
 
-                var newStageData = obj.transform.Find("StageData").GetComponent<StageData>();
-                newStageData.Deserialize(data);
+                Debug.Log("lines.Length=" + lines.Length);
+                Debug.Log("index1=" + index1 + ", index2=" + index2);
+                Debug.Log("data1=" + data1 + ", data2=" + data2);
 
-                Debug.Log("BUILD: " + newStageData.Serialize());
+                var obj1 = pool.TryToSpawn();
+                var obj2 = pool.TryToSpawn();
+
+                if (obj1 != null && obj2 != null)
+                {
+                    obj1.transform.SetPositionAndRotation(spawn.position + new Vector3(-0.15f, 0.15f, 0.0f), spawn.rotation);
+                    obj2.transform.SetPositionAndRotation(spawn.position + new Vector3(0.15f, -0.15f, 0.0f), spawn.rotation);
+
+                    var sync1 = (VRCObjectSync)obj1.GetComponent(typeof(VRCObjectSync));
+                    sync1.FlagDiscontinuity();
+
+                    var sync2 = (VRCObjectSync)obj2.GetComponent(typeof(VRCObjectSync));
+                    sync2.FlagDiscontinuity();
+
+                    var newStageData1 = obj1.transform.Find("StageData").GetComponent<StageData>();
+                    newStageData1.Deserialize(data1);
+
+                    var newStageData2 = obj2.transform.Find("StageData").GetComponent<StageData>();
+                    newStageData2.Deserialize(data2);
+
+                    Debug.Log("BUILD: " + newStageData1.Serialize());
+                    Debug.Log("BUILD: " + newStageData2.Serialize());
+                }
+                else
+                {
+                    if (obj1 != null)
+                    {
+                        pool.Return(obj1);
+                    }
+
+                    if (obj2 != null)
+                    {
+                        pool.Return(obj2);
+                    }
+                }
+            }
+            else
+            {
+                var data = GetRandomData();
+
+                var obj = pool.TryToSpawn();
+
+                if (obj != null)
+                {
+                    obj.transform.SetPositionAndRotation(spawn.position, spawn.rotation);
+
+                    var sync = (VRCObjectSync)obj.GetComponent(typeof(VRCObjectSync));
+                    sync.FlagDiscontinuity();
+
+                    var newStageData = obj.transform.Find("StageData").GetComponent<StageData>();
+                    newStageData.Deserialize(data);
+
+                    Debug.Log("BUILD: " + newStageData.Serialize());
+                }
             }
         }
     }
